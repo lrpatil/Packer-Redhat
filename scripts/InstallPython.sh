0adc282174aabfb4971d979f5b77b535dc7a1869
lrpatil <patillaxmikant72@gmail.com>
2019-07-24 17:44:41 +0530

    changed content

--- scripts/InstallPython.sh
+++ scripts/InstallPython.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo yum install python36 -y
+sudo yum install python37 -y
 sudo echo 'alias python="/usr/bin/python36"' >> ~/.bashrc
 sudo echo 'export PATH=/usr/bin:$PATH' >> ~/.bashrc
